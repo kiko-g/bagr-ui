8753c24075b4323757cb9280058a93bf6eaa2e6d
kiko-g <kikojpgoncalves@gmail.com>
2024-04-15 01:27:54 +0100

    remove loading component for code copy

--- components/ComponentShowcase.tsx
+++ components/ComponentShowcase.tsx
@@ -80,8 +80,6 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
               <LinkToGithubButton path={path} />
             </div>
 
-            <Loading code={code} />
-
             <Transition
               show={isCodeVisible}
               enter="transition-all duration-75"
@@ -123,28 +121,6 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
         </Transition>
       </Disclosure>
     </li>
-  )
-}
-
-function Loading({ code }: { code: string }) {
-  if (code !== "") return
-
-  return (
-    <div className="flex w-full items-center justify-center bg-[#1E2937] px-8 py-16 shadow dark:bg-black/10">
-      <svg
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        className="-ml-1 mr-3 h-12 w-12 animate-spin text-primary dark:text-secondary"
-      >
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
   )
 }
 
