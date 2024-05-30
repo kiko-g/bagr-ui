77eafb91875f4100d44576f60448e8f94500c215
kiko-g <kikojpgoncalves@gmail.com>
2024-05-30 10:45:33 +0100

    fix code showcase

--- components/ComponentShowcase.tsx
+++ components/ComponentShowcase.tsx
@@ -5,7 +5,7 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import { Inter_Tight } from "next/font/google"
 import { GithubIcon } from "./icons/GithubIcon"
-import { Disclosure, Switch, Transition } from "@headlessui/react"
+import { Disclosure, DisclosureButton, DisclosurePanel, Switch, Transition } from "@headlessui/react"
 import {
   CheckIcon,
   ClipboardIcon,
@@ -47,7 +47,7 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
   return (
     <li className="flex flex-col">
       <Disclosure defaultOpen={isOpen}>
-        <Disclosure.Button
+        <DisclosureButton
           onClick={() => setIsOpen((prev) => !prev)}
           className={clsx(
             inter.className,
@@ -57,7 +57,7 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
         >
           <span>{name}</span>
           {isOpen ? <MinusIcon className="h-5 w-5" /> : <PlusIcon className="h-5 w-5" />}
-        </Disclosure.Button>
+        </DisclosureButton>
 
         <Transition
           show={isOpen}
@@ -68,7 +68,7 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
           leaveFrom="transform scale-100 opacity-100"
           leaveTo="transform scale-95 opacity-0"
         >
-          <Disclosure.Panel className={clsx("relative", isOpen ? "mb-8" : "mb-0")}>
+          <DisclosurePanel className={clsx("relative", isOpen ? "mb-8" : "mb-0")}>
             <div className="absolute right-4 top-4 z-10 flex items-center justify-end gap-2">
               <CopyCodeButton text={code} />
               <ChangeViewModeButton isCodeVisible={isCodeVisible} toggle={() => setIsCodeVisible((prev) => !prev)} />
@@ -78,15 +78,7 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
               <LinkToGithubButton path={path} />
             </div>
 
-            <Transition
-              show={isCodeVisible}
-              enter="transition-all duration-75"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-all duration-150"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
+            {isCodeVisible ? (
               <SyntaxHighlighter
                 language="tsx"
                 showLineNumbers
@@ -102,22 +94,12 @@ export function ComponentShowcase({ name, path, collapseAll = false, Component }
               >
                 {code}
               </SyntaxHighlighter>
-            </Transition>
-
-            <Transition
-              show={!isCodeVisible}
-              enter="transition-all duration-150"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-all duration-75"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
+            ) : (
               <div className="flex w-full items-center justify-center rounded-b bg-slate-150 px-8 py-16 dark:bg-black/20">
                 {Component}
               </div>
-            </Transition>
-          </Disclosure.Panel>
+            )}
+          </DisclosurePanel>
         </Transition>
       </Disclosure>
     </li>
